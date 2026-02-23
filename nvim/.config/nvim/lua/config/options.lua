c7a0225f4fd6373484890ee89fee293c67f0947d
tomerfi1210 <tomerfisher@ip-10-100-102-28.us-west-2.compute.internal>
2026-02-23 21:41:48 +0200

    improve config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,11 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+local cwd = vim.uv.cwd()
+if not cwd or not vim.uv.fs_stat(cwd) then
+  vim.cmd.cd(vim.fn.expand("~"))
+end
+
 vim.g.lazyvim_python_lsp = "basedpyright"
 local opt = vim.opt
 opt.wrap = true
